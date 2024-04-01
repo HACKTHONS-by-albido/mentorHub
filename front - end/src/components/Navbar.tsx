c4459e067be288f86d1ef4e7e092ae7806ed0772
Harikrishnanmangeeri <hkmangeeri@gmail.com>
2024-04-02 00:39:31 +0530

    logo on nav bar

--- front - end/src/components/Navbar.tsx
+++ front - end/src/components/Navbar.tsx
@@ -1,19 +1,19 @@
-'use client'
-import Image from 'next/image';
-import React, { useState } from 'react';
-// import { useToast } from "@/components/ui/use-toast";
-
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { IoChatbubblesSharp } from "react-icons/io5";
+import { IoNotifications } from "react-icons/io5";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-// const {toast} = useToast()
+  const [hasNotifications, setHasNotifications] = useState(false); // State to track notifications
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   const handleLogout = () => {
     // Handle logout logic
-    // toast({description:'Logout successful'});
   };
 
   return (
@@ -22,28 +22,97 @@ const Navbar = () => {
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center">
             <a href="/" className="flex items-center space-x-3">
-              <Image src="/Mentor.png" height={40} width={40} className="h-8" alt="mentorHub" />
-              <span className="text-xl font-semibold text-gray-800 dark:text-white">mentorHub</span>
+              <Image
+                src="/Mentor.png"
+                height={40}
+                width={40}
+                className="h-8"
+                alt="mentorHub"
+              />
+              <span className="text-xl font-semibold text-gray-800 dark:text-white">
+                mentorHub
+              </span>
             </a>
           </div>
           <div className="hidden md:flex items-center space-x-4">
-            <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white">Home</a>
-            <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white">About</a>
-            <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white">Services</a>
-            <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white">Contact</a>
+            <a
+              href="#"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white"
+            >
+              Services
+            </a>
+            <a
+              href="#"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white"
+            >
+              Contact
+            </a>
           </div>
           <div className="hidden md:flex items-center space-x-4">
-            <button onClick={handleLogout} className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white focus:outline-none">Logout</button>
-            <Image src="/avatar.png" height={40} width={40} className="rounded-full" alt="User Avatar" />
+            <button className="relative">
+              <IoNotifications />
+              {hasNotifications && (
+                <span className="absolute top-0 right-0 w-3 h-3 bg-red-500 rounded-full"></span>
+              )}
+            </button>
+
+            <button>
+              <IoChatbubblesSharp />
+            </button>
+            <button
+              onClick={handleLogout}
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white focus:outline-none"
+            >
+              Logout
+            </button>
+            <Image
+              src="/avatar.png"
+              height={40}
+              width={40}
+              className="rounded-full"
+              alt="User Avatar"
+            />
           </div>
           <div className="flex items-center md:hidden">
-            <button onClick={toggleMenu} type="button" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white focus:outline-none">
-              <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white focus:outline-none"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
               </svg>
             </button>
             <div className="flex justify-end">
-              <Image src="/avatar.png" height={40} width={40} className="rounded-full" alt="User Avatar" />
+              <Image
+                src="/avatar.png"
+                height={40}
+                width={40}
+                className="rounded-full"
+                alt="User Avatar"
+              />
             </div>
           </div>
         </div>
@@ -51,11 +120,36 @@ const Navbar = () => {
       {isMenuOpen && (
         <div className="md:hidden bg-white dark:bg-gray-900">
           <div className="flex flex-col space-y-4 px-4 py-2">
-            <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white">Home</a>
-            <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white">About</a>
-            <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white">Services</a>
-            <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white">Contact</a>
-            <button onClick={handleLogout} className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white focus:outline-none">Logout</button>
+            <a
+              href="#"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white"
+            >
+              Services
+            </a>
+            <a
+              href="#"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white"
+            >
+              Contact
+            </a>
+            <button
+              onClick={handleLogout}
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white focus:outline-none"
+            >
+              Logout
+            </button>
           </div>
         </div>
       )}
