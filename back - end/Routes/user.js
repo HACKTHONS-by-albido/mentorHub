a12814037ff51aca45e9e11debd3a8481593931b
RisvanBCA2022 <111649025+RisvanBCA2022@users.noreply.github.com>
2024-04-05 23:32:55 +0530

    Addititonal details api function

--- back - end/Routes/user.js
+++ back - end/Routes/user.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const userRoutes=express.Router()
-const {register, login, profile, getDetails, sendMessage}=require('../Controller/user')
+const {register, login, profile, getDetails, sendMessage, additionalDetails}=require('../Controller/user')
 const userJwtAuthentication = require('../middleware/userJwtAuthentication')
 
 userRoutes.post('/auth/register',register)
@@ -8,6 +8,7 @@ userRoutes.post('/auth/login',login)
 userRoutes.get('/profile',userJwtAuthentication,profile)
 userRoutes.get('/getDetails/:id',userJwtAuthentication,getDetails)
 userRoutes.post('/sendMessage',userJwtAuthentication,sendMessage)
+userRoutes.post('/additionaldetails',userJwtAuthentication,additionalDetails)
 
 
 
