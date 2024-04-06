3bf7b27fb620099c9f8f74354660441ca7deba36
irfanpm <muhammmedirfanpm@gmail.com>
2024-04-06 09:44:50 +0530

    mentors list

---
+++ front - end/src/app/mentors/page.tsx
@@ -0,0 +1,12 @@
+import Mentorslist from '@/components/mentorslist'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Mentorslist/>
+    </div>
+  )
+}
+
+export default page
