2eb105a6acb0dc386d4f8c34515c3740289e7392
RisvanBCA2022 <111649025+RisvanBCA2022@users.noreply.github.com>
2024-04-07 17:50:01 +0530

    update

--- front - end/src/app/user/page.tsx
+++ front - end/src/app/user/page.tsx
@@ -1,5 +1,5 @@
-import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/mentee/Footer'
 import React from 'react'
 
 const page = () => {
